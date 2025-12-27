bce4d541f74ebfec50520c4284704c4fb955be89
Ivan Georgiev <ivan.georgiev0904@gmail.com>
2025-12-27 16:05:56 +0200

    Add exception for not found entities

---
+++ core/src/main/java/com/ivangeorgiev/domuapi/core/exceptions/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.ivangeorgiev.domuapi.core.exceptions;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
