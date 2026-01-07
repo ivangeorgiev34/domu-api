3cc3529db0616a1a7d008ef9d180bb31c1fa7489
Ivan Georgiev <ivan.georgiev0904@gmail.com>
2026-01-08 00:03:46 +0200

    Add starting class

--- web/src/main/java/com/ivangeorgiev/domuapi/web/MyApplication.java
+++ web/src/main/java/com/ivangeorgiev/domuapi/web/MyApplication.java
@@ -2,12 +2,16 @@ package com.ivangeorgiev.domuapi.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {
         "com.ivangeorgiev.domuapi.web",
         "com.ivangeorgiev.domuapi.core",
         "com.ivangeorgiev.domuapi.data"
 })
+@EnableJpaRepositories("com.ivangeorgiev.domuapi.data.repositories")
+@EntityScan("com.ivangeorgiev.domuapi.data.models")
 public class MyApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class, args);
